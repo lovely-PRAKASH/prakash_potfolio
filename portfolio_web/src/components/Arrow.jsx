2aafa8b56a2c27aa76a1e124f9de1b8679354bb6
prakash-2002 <prakashslm27@gmail.com>
2024-10-25 22:02:03 +0530

    arrow component created for travel bottom to top

---
+++ portfolio_web/src/components/Arrow.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IoIosArrowUp } from "react-icons/io";
+
+const Arrow = () => {
+  return (
+    <div className='arrow-div'>
+    <a href="#home" className='arrow' >
+    <IoIosArrowUp />
+    </a>
+
+</div>
+  )
+}
+
+export default Arrow
